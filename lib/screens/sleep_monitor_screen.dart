154011376c0c16d84ffcb2c6c22b5f4eda241891
Shanaaz Ahamed <77115237+imshaaz21@users.noreply.github.com>
2024-04-27 22:46:42 +0530

    Add proximity sensor functionality to Sleep Monitor Screen

--- lib/screens/sleep_monitor_screen.dart
+++ lib/screens/sleep_monitor_screen.dart
@@ -1,7 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' as foundation;
+import 'dart:async';
+import 'package:proximity_sensor/proximity_sensor.dart';
 
-class SleepMonitorScreen extends StatelessWidget {
+class SleepMonitorScreen extends StatefulWidget {
   const SleepMonitorScreen({super.key});
+
+  @override
+  State<SleepMonitorScreen> createState() => _SleepMonitorScreenState();
+}
+
+class _SleepMonitorScreenState extends State<SleepMonitorScreen> {
+  bool _isNear = false;
+  double _proximity = 0;
+  late StreamSubscription<dynamic> _streamSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    listenSensor();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _streamSubscription.cancel();
+  }
+
+  Future<void> listenSensor() async {
+    debugPrint('listening to sensor...');
+    FlutterError.onError = (FlutterErrorDetails details) {
+      if (foundation.kDebugMode) {
+        FlutterError.dumpErrorToConsole(details);
+      }
+    };
+
+    // --------------------------------------------------------------------
+    // You only need to make this call if you want to turn off the screen.
+    await ProximitySensor.setProximityScreenOff(true)
+        .onError((error, stackTrace) {
+      debugPrint("could not enable screen off functionality");
+      return null;
+    });
+
+    _streamSubscription = ProximitySensor.events.listen((int event) {
+      debugPrint('Proximity: $event');
+      setState(() {
+        _isNear = (event > 0) ? true : false;
+        _proximity = event.toDouble();
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,12 +58,25 @@ class SleepMonitorScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Sleep Monitor'),
       ),
-      body: const Column(
+      body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
             child: Center(
-              child: Text('Sleep Monitor Screen'),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Proximity: $_proximity',
+                    style: const TextStyle(fontSize: 24),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    'Is Near: $_isNear',
+                    style: const TextStyle(fontSize: 24),
+                  ),
+                ],
+              ),
             ),
           )
         ],
