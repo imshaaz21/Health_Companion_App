bbd4cc12c5f0814d7438ec072de07e360fdf7dfe
Shanaaz Ahamed <77115237+imshaaz21@users.noreply.github.com>
2024-04-28 12:07:09 +0530

    Add CaloriesCard widget for displaying calories burned in the UI

---
+++ lib/widgets/calories_card.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class CaloriesCard extends StatelessWidget {
+  const CaloriesCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int caloriesBurned = 300;
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      surfaceTintColor: Colors.white,
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Calories',
+              style: TextStyle(fontSize: 18, color: Colors.grey[600]),
+            ),
+            const SizedBox(height: 10),
+            Center(
+              child: Column(
+                children: [
+                  const Icon(
+                    Icons.whatshot,
+                    color: Colors.orange,
+                    size: 60,
+                  ),
+                  const SizedBox(width: 10),
+                  Text(
+                    '$caloriesBurned',
+                    style: const TextStyle(
+                        fontSize: 32, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 15),
+            const Expanded(
+              child: Center(
+                child: Text(
+                  'Today',
+                  style: TextStyle(fontSize: 12, color: Colors.grey),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
