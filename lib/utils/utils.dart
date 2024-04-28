46f6d5421a030975c1834fec246d28a799c97067
Shanaaz Ahamed <77115237+imshaaz21@users.noreply.github.com>
2024-04-28 21:53:06 +0530

    Add formatDateForProvider function to utils.dart

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,3 +1,8 @@
 String formatDate(DateTime d) {
   return d.toString().substring(0, 19);
 }
+
+// formate date for provider yyyy-mm-dd
+String formatDateForProvider(DateTime d) {
+  return '${d.year}-${d.month}-${d.day}';
+}
