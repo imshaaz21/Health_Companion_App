b0bfcdf0bbadeabdb16ff308f897202a6da60e94
hafeelnm19 <hafeelnm.19@uom.lk>
2024-04-28 18:49:55 +0530

    sleep monitoring accelerometer

--- lib/screens/sleep_monitor_screen.dart
+++ lib/screens/sleep_monitor_screen.dart
@@ -22,6 +22,8 @@ class Accelerometer extends State<SleepMonitorScreen> {
   String _luxString = 'Unknown';
   Light? _light;
   StreamSubscription? _subscription;
+  double _activityThreshold = 0.5;
+  Duration _activity_last_slept_time = Duration(seconds: 0);
 
   // Accelerometer  subscription
   List<AccelerometerEvent> _accelerometerValues = [];
@@ -53,11 +55,46 @@ class Accelerometer extends State<SleepMonitorScreen> {
     listenSensor();
     startListeningLight();
 
-    _accelerometerSubscription = accelerometerEventStream().listen((event) {
-      setState(() {
-        // Update the _accelerometerValues list with the latest event
-        _accelerometerValues = [event];
-      });
+    _accelerometerSubscription =
+        accelerometerEvents.listen((AccelerometerEvent event) {
+      double totalAcceleration = event.y.abs();
+      if (totalAcceleration > _activityThreshold) {
+        // Movement detected, potentially not sleeping
+        if (_isSleeping) {
+          // Person was sleeping, but now moving
+          // Reset sleep tracking
+          setState(() {
+            _isSleeping = false;
+            _sleepStartTime = null;
+            if (_sleepDuration > 10) {
+              _activity_last_slept_time = Duration(seconds: _sleepDuration);
+            }
+            _sleepDuration = 0;
+          });
+        }
+      } else {
+        // No significant movement, potentially sleeping
+        if (!_isSleeping) {
+          // Start sleep tracking
+          _sleepStartTime = DateTime.now();
+          _isSleeping = true;
+        } else {
+          // Check if sleep duration is more than one hour
+          if (_sleepStartTime != null &&
+              DateTime.now().difference(_sleepStartTime!) >=
+                  const Duration(seconds: 10)) {
+            // Person is sleeping for more than one hour
+            // Consider it as sleeping
+            Duration sleepDuration =
+                DateTime.now().difference(_sleepStartTime!);
+            debugPrint(
+                'Person is sleeping for more than one hour: $sleepDuration');
+            setState(() {
+              _sleepDuration = sleepDuration.inSeconds;
+            });
+          }
+        }
+      }
     });
   }
 
@@ -78,39 +115,39 @@ class Accelerometer extends State<SleepMonitorScreen> {
 
     _streamSubscription = ProximitySensor.events.listen((int event) {
       debugPrint('Proximity: $event');
-      setState(() {
-        _proximity = event.toDouble();
-        _isNear = (_proximity > 0);
-        if (_isNear) {
-          // Proximity sensor is near, potentially sleeping
-          if (!_isSleeping) {
-            // Start sleep tracking
-            _sleepStartTime = DateTime.now();
-            _isSleeping = true;
-          } else {
-            // Check if sleep duration is more than one hour
-            if (_sleepStartTime != null &&
-                DateTime.now().difference(_sleepStartTime!) >=
-                    const Duration(seconds: 10)) {
-              // Person is sleeping for more than one hour
-              // Consider it as sleeping
-              // Perform your logic here, for example, show sleep duration
-              Duration sleepDuration =
-                  DateTime.now().difference(_sleepStartTime!);
-              debugPrint(
-                  'Person is sleeping for more than one hour: $sleepDuration');
-              setState(() {
-                _sleepDuration = sleepDuration.inSeconds;
-              });
-              // You can perform any action here, such as showing sleep duration
-            }
-          }
-        } else {
-          // Proximity sensor is not near, waking up
-          _isSleeping = false;
-          _sleepStartTime = null; // Reset sleep start time
-        }
-      });
+      //   setState(() {
+      //     _proximity = event.toDouble();
+      //     _isNear = (_proximity > 0);
+      //     if (_isNear) {
+      //       // Proximity sensor is near, potentially sleeping
+      //       if (!_isSleeping) {
+      //         // Start sleep tracking
+      //         _sleepStartTime = DateTime.now();
+      //         _isSleeping = true;
+      //       } else {
+      //         // Check if sleep duration is more than one hour
+      //         if (_sleepStartTime != null &&
+      //             DateTime.now().difference(_sleepStartTime!) >=
+      //                 const Duration(seconds: 10)) {
+      //           // Person is sleeping for more than one hour
+      //           // Consider it as sleeping
+      //           // Perform your logic here, for example, show sleep duration
+      //           Duration sleepDuration =
+      //               DateTime.now().difference(_sleepStartTime!);
+      //           debugPrint(
+      //               'Person is sleeping for more than one hour: $sleepDuration');
+      //           setState(() {
+      //             _sleepDuration = sleepDuration.inSeconds;
+      //           });
+      //           // You can perform any action here, such as showing sleep duration
+      //         }
+      //       }
+      //     } else {
+      //       // Proximity sensor is not near, waking up
+      //       _isSleeping = false;
+      //       _sleepStartTime = null; // Reset sleep start time
+      //     }
+      //   });
     });
   }
 
@@ -148,6 +185,11 @@ class Accelerometer extends State<SleepMonitorScreen> {
                   const SizedBox(height: 20),
                   Text(
                     'Lux: $_luxString',
+                    style: const TextStyle(fontSize: 24),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    'Last sleep Time: $_activity_last_slept_time',
                     style: const TextStyle(fontSize: 24),
                   ),
                   Card(
