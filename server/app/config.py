82b4985669812c554ea98f1806425bc9ab05efba
Shanaaz Ahamed <77115237+imshaaz21@users.noreply.github.com>
2024-05-01 11:58:35 +0530

    Update file paths in config.py to use relative paths instead of absolute paths

--- server/app/config.py
+++ server/app/config.py
@@ -1,13 +1,13 @@
 # config.py
 UPLOAD_FOLDER = "uploads"
-TEMP_FOLDER = "/home/akshilmy/MobileApps/health_companion_app/server/temp"
+TEMP_FOLDER = "D:/CSE/CS4472 - Mobile Computing/health_companion_app/server/temp"
 DEFAULT_FPS = 10
-VIDEO_OUTPUT_PATH = "/home/akshilmy/MobileApps/health_companion_app/server/output"
+VIDEO_OUTPUT_PATH = "D:/CSE/CS4472 - Mobile Computing/health_companion_app/server/output"
 
 
 QUESTIONNAIRES = [
-    '/home/akshilmy/MobileApps/health_companion_app/server/app/questionnaire/image_feedback.json',
-    '/home/akshilmy/MobileApps/health_companion_app/server/app/questionnaire/mental_health.json',
-    '/home/akshilmy/MobileApps/health_companion_app/server/app/questionnaire/jokes.json',
+    'D:/CSE/CS4472 - Mobile Computing/health_companion_app/server/app/questionnaire/image_feedback.json',
+    'D:/CSE/CS4472 - Mobile Computing/health_companion_app/server/app/questionnaire/mental_health.json',
+    'D:/CSE/CS4472 - Mobile Computing/health_companion_app/server/app/questionnaire/jokes.json',
 ]
 NUM_QUESTIONS = 10
