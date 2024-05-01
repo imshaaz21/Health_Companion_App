36183f838de372043db76b9dfdcbc5d1e65b12cf
Shanaaz Ahamed <77115237+imshaaz21@users.noreply.github.com>
2024-05-01 09:39:17 +0530

    Remove commented out code and unused variables in SleepMonitorProvider

--- lib/providers/sleep_monitor_provider.dart
+++ lib/providers/sleep_monitor_provider.dart
@@ -3,57 +3,19 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class SleepMonitorProvider with ChangeNotifier {
   String _sleepDuration = '0';
-  // String _lightCondition = 'Unknown';
-  // double _luxValue = 0.0;
   Map<String, double> _sleepHistory = {};
 
   String get sleepDuration => _sleepDuration;
-  // String get lightCondition => _lightCondition;
-  // double get luxValue => _luxValue;
-
   Map<String, double> get sleepHistory => _sleepHistory;
 
   SleepMonitorProvider() {
     loadSleepHistory();
-    // resetSleepHistory();
   }
 
   void updateSleepDuration(String newDuration) {
     _sleepDuration = newDuration;
     notifyListeners();
   }
-
-  // void updateLuxValue(double luxValue) {
-  //   _setLightCondition(luxValue);
-  //   _luxValue = luxValue;
-  //   notifyListeners();
-  // }
-
-  // void _setLightCondition(double luxValue) {
-  //   if (luxValue >= 50000) {
-  //     _lightCondition = 'British Summer sunshine';
-  //   } else if (luxValue >= 10000 && luxValue < 50000) {
-  //     _lightCondition = 'Ambient Daylight';
-  //   } else if (luxValue >= 1000 && luxValue < 10000) {
-  //     _lightCondition = 'Overcast daylight';
-  //   } else if (luxValue >= 500 && luxValue < 1000) {
-  //     _lightCondition = 'Well lit office';
-  //   } else if (luxValue >= 400 && luxValue < 500) {
-  //     _lightCondition = 'Sunset & Sunrise';
-  //   } else if (luxValue >= 120 && luxValue < 400) {
-  //     _lightCondition = 'Family living room';
-  //   } else if (luxValue >= 100 && luxValue < 120) {
-  //     _lightCondition = 'Lifts';
-  //   } else if (luxValue >= 15 && luxValue < 100) {
-  //     _lightCondition = 'Street lightning';
-  //   } else if (luxValue >= 1 && luxValue < 15) {
-  //     _lightCondition = 'Moonlight (full moon)';
-  //   } else if (luxValue >= 0.1 && luxValue < 1) {
-  //     _lightCondition = 'Night (No moon)';
-  //   } else {
-  //     _lightCondition = 'Unknown';
-  //   }
-  // }
 
   Future<void> loadSleepHistory() async {
     final pref = await SharedPreferences.getInstance();
