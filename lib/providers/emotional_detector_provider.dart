ccd8f8dd9eb1b62b9cccfb457301705ab85dd861
Shanaaz Ahamed <77115237+imshaaz21@users.noreply.github.com>
2024-05-01 09:39:10 +0530

    Add EmotionalDetectorProvider class for managing emotional test results

---
+++ lib/providers/emotional_detector_provider.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:health_companion_app/models/emotional_test_data.dart';
+import 'package:health_companion_app/utils/utils.dart';
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class EmotionalDetectorProvider with ChangeNotifier {
+  List<EmotionalTestResult> _testResults = [];
+
+  List<EmotionalTestResult> get testResults => _testResults;
+
+  EmotionalDetectorProvider() {
+    loadTestResults();
+  }
+
+  Future<void> loadTestResults() async {
+    final pref = await SharedPreferences.getInstance();
+    final testResultsJson = pref.getStringList('emotionalTestResults') ?? [];
+    _testResults = testResultsJson.map((json) {
+      final Map<String, dynamic> data = jsonDecode(json);
+      return EmotionalTestResult(
+        testDate: DateTime.parse(data['testDate']),
+        score: data['score'],
+        mood: data['mood'],
+      );
+    }).toList();
+
+    // Add dummy data if there are less than 5 test results
+    if (_testResults.length < 5) {
+      // Add 5 dummy data entries
+      for (int i = 0; i < 5 - _testResults.length; i++) {
+        _testResults.add(EmotionalTestResult(
+          testDate: DateTime.now().subtract(Duration(days: i + 1)),
+          score: 0.0,
+          mood: 'Neutral',
+        ));
+      }
+    }
+
+    debugPrint('EmotionalDetectorProvider loaded data : $_testResults');
+    notifyListeners();
+  }
+
+  Future<void> addTestResult(EmotionalTestResult result) async {
+    _testResults.add(result);
+    await _saveTestResults();
+    notifyListeners();
+  }
+
+  double getLatestTestScore() {
+    if (_testResults.isNotEmpty) {
+      return _testResults.last.score;
+    } else {
+      return 0.0;
+    }
+  }
+
+  String? getLatestTestDate() {
+    if (_testResults.isNotEmpty) {
+      return formatDateForProvider(_testResults.last.testDate);
+    } else {
+      return null;
+    }
+  }
+
+  String? getLatestMood() {
+    if (_testResults.isNotEmpty) {
+      return _testResults.last.mood;
+    } else {
+      return null;
+    }
+  }
+
+  Future<void> resetTestResults() async {
+    _testResults.clear();
+    await _saveTestResults();
+    notifyListeners();
+  }
+
+  Future<void> _saveTestResults() async {
+    final pref = await SharedPreferences.getInstance();
+    final testResultsJson = _testResults.map((result) {
+      return jsonEncode({
+        'testDate': result.testDate.toIso8601String(),
+        'score': result.score,
+        'mood': result.mood,
+      });
+    }).toList();
+    await pref.setStringList('emotionalTestResults', testResultsJson);
+  }
+}
